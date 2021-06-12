4a2ac8a1906dfa18625ca485138c70379f851b5b
Heidy19 <80176593+Heidy19@users.noreply.github.com>
2021-06-11 19:54:04 -0600

    Add files via upload

---
+++ G7_07_Heidy_PROYECTO/Etapa 2 -BDD + DATOS/G7_07_Heidy_DB_PROYECTO_FINAL.sql
@@ -0,0 +1,229 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-06-2021 a las 21:11:54
+-- Versión del servidor: 10.4.18-MariaDB
+-- Versión de PHP: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_tienda_sadashi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_carrito`
+--
+
+CREATE TABLE `tbl_carrito` (
+  `ID_CARRITO` int(11) NOT NULL,
+  `ID_CLIENTE` int(11) NOT NULL,
+  `ID_PRODUCTO` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_categorias`
+--
+
+CREATE TABLE `tbl_categorias` (
+  `ID_CATEGORIAS` int(11) NOT NULL,
+  `CATEGORIAS` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_cupones`
+--
+
+CREATE TABLE `tbl_cupones` (
+  `ID_CUPON` int(11) NOT NULL,
+  `CODIGO` varchar(50) NOT NULL,
+  `ID_CLIENTE` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_pedido`
+--
+
+CREATE TABLE `tbl_pedido` (
+  `ID_PEDIDO` int(11) NOT NULL,
+  `TELEFONO` int(11) NOT NULL,
+  `FORMA_DE_PAGO` int(11) NOT NULL,
+  `TOTAL_A_PAGAR` int(11) NOT NULL,
+  `DIRECCION` varchar(50) NOT NULL,
+  `ID_PRODUCTO` int(11) NOT NULL,
+  `ID_CLIENTE` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_producto`
+--
+
+CREATE TABLE `tbl_producto` (
+  `ID_PRODUCTO` int(11) NOT NULL,
+  `PRODUCTO` varchar(50) NOT NULL,
+  `TALLA` varchar(50) NOT NULL,
+  `PRECIO` int(50) NOT NULL,
+  `ID_CATEGORIA` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tlb_cliente`
+--
+
+CREATE TABLE `tlb_cliente` (
+  `ID_CLIENTE` int(11) NOT NULL,
+  `CORREO` varchar(100) NOT NULL,
+  `CLAVE` varchar(100) NOT NULL,
+  `NOMBRE` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tlb_cliente`
+--
+
+INSERT INTO `tlb_cliente` (`ID_CLIENTE`, `CORREO`, `CLAVE`, `NOMBRE`) VALUES
+(1, 'heidy@4821', '123', 'HEIDY'),
+(2, 'hdowi@3124', '1234', 'JOSE');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tbl_carrito`
+--
+ALTER TABLE `tbl_carrito`
+  ADD PRIMARY KEY (`ID_CARRITO`),
+  ADD KEY `FK_CLIENTE3` (`ID_CLIENTE`),
+  ADD KEY `FK_PRODUCTO2` (`ID_PRODUCTO`);
+
+--
+-- Indices de la tabla `tbl_categorias`
+--
+ALTER TABLE `tbl_categorias`
+  ADD PRIMARY KEY (`ID_CATEGORIAS`);
+
+--
+-- Indices de la tabla `tbl_cupones`
+--
+ALTER TABLE `tbl_cupones`
+  ADD PRIMARY KEY (`ID_CUPON`),
+  ADD KEY `FK_CLIENTE` (`ID_CLIENTE`);
+
+--
+-- Indices de la tabla `tbl_pedido`
+--
+ALTER TABLE `tbl_pedido`
+  ADD PRIMARY KEY (`ID_PEDIDO`),
+  ADD KEY `FK_PRODUCTO1` (`ID_PRODUCTO`),
+  ADD KEY `FK_CLIENTE5` (`ID_CLIENTE`);
+
+--
+-- Indices de la tabla `tbl_producto`
+--
+ALTER TABLE `tbl_producto`
+  ADD PRIMARY KEY (`ID_PRODUCTO`),
+  ADD KEY `FK_CATEGORIA` (`ID_CATEGORIA`);
+
+--
+-- Indices de la tabla `tlb_cliente`
+--
+ALTER TABLE `tlb_cliente`
+  ADD PRIMARY KEY (`ID_CLIENTE`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_carrito`
+--
+ALTER TABLE `tbl_carrito`
+  MODIFY `ID_CARRITO` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_categorias`
+--
+ALTER TABLE `tbl_categorias`
+  MODIFY `ID_CATEGORIAS` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_cupones`
+--
+ALTER TABLE `tbl_cupones`
+  MODIFY `ID_CUPON` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_pedido`
+--
+ALTER TABLE `tbl_pedido`
+  MODIFY `ID_PEDIDO` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_producto`
+--
+ALTER TABLE `tbl_producto`
+  MODIFY `ID_PRODUCTO` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tlb_cliente`
+--
+ALTER TABLE `tlb_cliente`
+  MODIFY `ID_CLIENTE` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `tbl_carrito`
+--
+ALTER TABLE `tbl_carrito`
+  ADD CONSTRAINT `FK_CATEGORIA3` FOREIGN KEY (`ID_CLIENTE`) REFERENCES `tlb_cliente` (`ID_CLIENTE`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_PRODUCTO2` FOREIGN KEY (`ID_PRODUCTO`) REFERENCES `tbl_producto` (`ID_PRODUCTO`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `tbl_cupones`
+--
+ALTER TABLE `tbl_cupones`
+  ADD CONSTRAINT `FK_CATEGORIA4` FOREIGN KEY (`ID_CLIENTE`) REFERENCES `tlb_cliente` (`ID_CLIENTE`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `tbl_pedido`
+--
+ALTER TABLE `tbl_pedido`
+  ADD CONSTRAINT `FK_CLIENTE5` FOREIGN KEY (`ID_CLIENTE`) REFERENCES `tlb_cliente` (`ID_CLIENTE`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_PRODUCTO1` FOREIGN KEY (`ID_PRODUCTO`) REFERENCES `tbl_producto` (`ID_PRODUCTO`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `tbl_producto`
+--
+ALTER TABLE `tbl_producto`
+  ADD CONSTRAINT `FK_CATEGORIA` FOREIGN KEY (`ID_CATEGORIA`) REFERENCES `tbl_categorias` (`ID_CATEGORIAS`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
